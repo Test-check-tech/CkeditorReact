4729c77624eb16e5bc3f8b7620765951f97acb65
Mahalingam Varagur Suriyanarayanan <mahalingam.s@CTL.local>
2020-01-29 12:24:22 +0530

    Add existing file

--- src/components/Ckeditor.js
+++ src/components/Ckeditor.js
@@ -18,7 +18,7 @@ class CKeditor extends Component {
                     } }
                     onChange={ ( event, editor ) => console.log( { event, editor } ) }
                     editor={ DecoupledEditor }
-                    data="Hello Ckeditor  ckeditor 5"
+                    data="Hello Ckeditor 5"
                    
                 />
             </div>
